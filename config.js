8e3c9b3564aaa7fda8521e4b6d60fd843f247a69
sangbeom <hhsb2441@gmail.com>
2023-01-13 15:17:33 +0900

    fix: 오타 수정

---
+++ config.js
@@ -0,0 +1,16 @@
+const config = {
+  db: {
+    username: "root",
+    password: "Hhsb4114!@",
+    host: "127.0.0.1",
+    port: "3306",
+    database: "community",
+    dialect: "mysql",
+    define: {
+      freezeTableName: true,
+      timestamps: false,
+    },
+  },
+};
+
+module.exports = config;
