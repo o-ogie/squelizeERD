7ec69eaf58b788f697069faeb1e9e727d38e261e
o-ogie <wrld.worthy@gmail.com>
2023-01-13 15:18:19 +0900

    feat: sequelize Board, Hashtag, Sort 구현

---
+++ models/board.model.js
@@ -0,0 +1,57 @@
+Board
+
+/*
+CREATE TABLE board(
+    boardIdx INT PRIMARY KEY AUTO_INCREMENT,
+    userid VARCHAR(30) FORIEGN KEY,
+    subject VARCHAR(50) NOT NULL,
+    contentBoard TEXT,
+    registerBoard DATETIME DEFAULT now(),
+    hit INT DEFAULT 0
+);
+*/
+
+module.exports = (sequelize, Sequelize) => {
+    class Board extends  Sequelize.Model {
+        static initailize() {
+            return this.init({
+                boardIdx:{
+                    type: Sequelize.INTEGER,
+                    primaryKey:true,
+                    autoIncrement:true,
+                },
+                userid:{
+                    type:Sequelize.STRING(30),
+                    allowNull: false,
+                },
+                subject:{
+                    type:Sequelize.STRING(50),
+                    allowNull:false,
+                },
+                contentBoard:{
+                    type:Sequelize.TEXT
+                },
+                registerBoard:{
+                    type:Sequelize.DATE,
+                    defaultValue:Sequelize.fn('now')
+                }
+            },{
+                sequelize
+            })
+        }
+
+        static associate(models){
+            // this.belongsTo(models.User,{
+            //     foreignKey:'userid'
+            // })
+
+            this.hasMany(models.Hashtag,{
+                foreignKey:'boardIdx',
+                constraints: true,
+                onDelete: 'cascade'
+            })
+        }
+    }
+    Board.initailize()
+    return Board
+}
---
+++ models/sort.model.js
@@ -0,0 +1,37 @@
+Sort
+
+/*
+    CREATE TABLE sort(
+        hashnum INT PRIMARY KEY AUTO_INCREMENT,
+        contenthash VARCHAR(30) NOT NULL
+    );
+*/
+
+module.exports = (sequelize,Sequelize) => {
+    class Sort extends Sequelize.Model {
+        static initialize(){
+            return this.init({
+                hashNum:{
+                    type:Sequelize.INTEGER,
+                    primaryKey:true,
+                    autoIncrement:true,
+                },
+                contentHash:{
+                    type:Sequelize.STRING(30),
+                    allowNull:false
+                }
+            },{
+                sequelize
+            })
+        }
+
+        static associate(models){
+            this.hasMany(models.Hashtag,{
+                foriegnKey:'hashNum',
+                foriegnKey:'contentHash'
+            })
+        }
+    }
+    Sort.initialize()
+    return Sort
+}
